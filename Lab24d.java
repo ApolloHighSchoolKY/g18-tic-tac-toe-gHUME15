8dcd390b16fdbc7ee757f656cff15a8766c25aa1
gHUME15 <Grayson.hume@stu.daviess.kyschools.us>
2025-02-19 19:18:16 +0000

    Not perfect, I need to fix i more latter

--- Lab24d.java
+++ Lab24d.java
@@ -14,15 +14,28 @@ public class Lab24d
 	{
 		Scanner file = new Scanner (new File("lab24d.dat"));
 
+		// Read how many times you should read
+		//Make a int varible file.nextint
+		// file.nextLine()
+		//Make a Tictactoe object for each game in the file
+		//Print the game(Tictaktoe object)
+		// Print the winer
 
-
-
-
-
-
-
+		 int numGames = file.nextInt();
+		 file.nextLine();
+ 
+		 for (int i = 0; i < numGames; i++) {
+			 String game = file.nextLine();
+ 
+			 TicTacToe ttt = new TicTacToe(game);
+ 
+			 System.out.println(ttt);
+ 
+			 System.out.println("Winner: " + ttt.getWinner());
+		 } file.close();
+		}
 	}
-}
+
 
 
 
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
@@ -13,44 +13,72 @@ public class TicTacToe
 
 	public TicTacToe()
 	{
+		mat = new char[5][5];
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				mat[i][j] = ' ';  
+		}
+		}
 
 
 	}
 
 	public TicTacToe(String game)
 	{
+		// Row major order will happed here
+		mat = new char[5][5];
 
-
+		for(int i = 0; i<5;i++){
+			for(int j=0; j<5;j++) {
+				mat[i][j] = game.charAt(i);
+	//mising inside the ()		
+			}
+		}
 
 
 	}
 
 	public String getWinner()
 	{
-
-
-
-
-
-
-
-
-
-
-
-
-		return "";
+		for (int i = 0; i < 5; i++) {
+			if (mat[i][0] != ' ' && mat[i][0] == mat[i][1] &&mat[i][1] ==mat[i][2] && mat[i][2] == mat[i][3] && mat[i][3]== mat[i][4]) {
+				return mat[i][0] + " Win";}
+		}
+		for (int j = 0; j < 5; j++) {
+			if (mat[0][j] != ' ' && mat[0][j] == mat[1][j] &&mat[1][j] ==mat[2][j] && mat[2][j] == mat[3][j] && mat[3][j] == mat[4][j]) {
+				return mat[0][j] + " Win";
+			}
+		}
+		if (mat[0][0] != ' ' && mat[0][0] == mat[1][1] &&mat[1][1] ==mat[2][2] && mat[2][2] == mat[3][3] && mat[3][3] == mat[4][4]) {
+			return mat[0][0] + " Win";
+		}
+	
+		
+		if (mat[0][4] != ' ' && mat[0][4] == mat[1][3] &&mat[1][3] ==mat[2][2] && mat[2][2] == mat[3][1] && mat[3][1] == mat[4][0]) {
+			return mat[0][4] + " Win"; 
+		}
+	
+		
+		return "No One Won";
 	}
 
-	public String toString()
-	{
-		String output="";
-
-
-
-
-
-
-		return output+"\n\n";
-	}
+	public String toString() {
+		String output = "";
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				output += mat[i][j];
+				if (j < 4) {
+					output += " | ";
+				}
+			}
+			output += "\n";
+	
+			if (i < 4) {
+				output += "--------------\n";
+			}
+		}
+	
+		return output;
+	} 
 }
